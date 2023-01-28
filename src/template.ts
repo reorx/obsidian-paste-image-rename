5221945394c25e5497e922f86d08813c572ccb6e
Sunghyun Cho <mail@chosunghyun.com>
2023-01-27 23:02:10 -0800

    apply suggstions

--- src/template.ts
+++ src/template.ts
@@ -1,49 +1,54 @@
-import {getRandomHex, getRandomUuid} from './generators'
-import { FrontMatterCache } from 'obsidian';
+import { getRandomHex } from "./generators";
+import { FrontMatterCache } from "obsidian";
 
-const dateTmplRegex = /{{DATE:([^}]+)}}/gm
-const frontmatterTmplRegex = /{{frontmatter:([^}]+)}}/gm
+const dateTmplRegex = /{{DATE:([^}]+)}}/gm;
+const frontmatterTmplRegex = /{{frontmatter:([^}]+)}}/gm;
 
 const replaceDateVar = (s: string, date: moment.Moment): string => {
-	const m = dateTmplRegex.exec(s)
-	if (!m) return s
-	return s.replace(m[0], date.format(m[1]))
-}
+	const m = dateTmplRegex.exec(s);
+	if (!m) return s;
+	return s.replace(m[0], date.format(m[1]));
+};
 
-const replaceFrontmatterVar = (s: string, frontmatter?: FrontMatterCache): string => {
-	if (!frontmatter) return s
-	const m = frontmatterTmplRegex.exec(s)
-	if (!m) return s
-	return s.replace(m[0], frontmatter[m[1]] || '')
-}
+const replaceFrontmatterVar = (
+	s: string,
+	frontmatter?: FrontMatterCache
+): string => {
+	if (!frontmatter) return s;
+	const m = frontmatterTmplRegex.exec(s);
+	if (!m) return s;
+	return s.replace(m[0], frontmatter[m[1]] || "");
+};
 
 interface TemplateData {
-	imageNameKey: string
-	fileName: string
-	dirName: string
-	firstHeading: string
+	imageNameKey: string;
+	fileName: string;
+	dirName: string;
+	firstHeading: string;
 }
 
-export const renderTemplate = (tmpl: string, data: TemplateData, frontmatter?: FrontMatterCache) => {
-	const now = window.moment()
-	let text = tmpl
-	let newtext
+export const renderTemplate = (
+	tmpl: string,
+	data: TemplateData,
+	frontmatter?: FrontMatterCache
+) => {
+	const now = window.moment();
+	let text = tmpl;
+	let newtext;
 	while ((newtext = replaceDateVar(text, now)) != text) {
-		text = newtext
+		text = newtext;
 	}
 	while ((newtext = replaceFrontmatterVar(text, frontmatter)) != text) {
-		text = newtext
+		text = newtext;
 	}
 
 	text = text
 		.replace(/{{imageNameKey}}/gm, data.imageNameKey)
 		.replace(/{{fileName}}/gm, data.fileName)
-		.replace(/{{uuid}}/gm, getRandomUuid(true, '-'))
-		.replace(/{{hex8}}/gm, getRandomHex(8, true))
-		.replace(/{{hex4}}/gm, getRandomHex(4, true))
-		.replace(/{{hex12}}/gm, getRandomHex(12, true))
-		.replace(/{{hex}}/gm, getRandomHex(6, true))		
+		.replace(/{{randomHex:([0-9]+)}}/gm, (s, n) => {
+			return getRandomHex(parseInt(n), true);
+		})
 		.replace(/{{dirName}}/gm, data.dirName)
-		.replace(/{{firstHeading}}/gm, data.firstHeading)
-	return text
-}
+		.replace(/{{firstHeading}}/gm, data.firstHeading);
+	return text;
+};
